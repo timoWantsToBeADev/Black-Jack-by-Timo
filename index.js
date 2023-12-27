aca2081a04bfe2049c5985a255f42041c7f49dcf
timoWantsToBeADev <99825941+timoWantsToBeADev@users.noreply.github.com>
2023-12-27 23:18:01 +0100

    uploaded
    uploaded

---
+++ index.js
@@ -0,0 +1,453 @@
+const newDeck = [{
+    card :  "2",
+    suit :  "hearts",
+    value : 2,
+    id : "100",
+    color : "red"
+},{
+    card :  "3",
+    suit :  "hearts",
+    value : 3,
+    id : "101",
+    color : "red"
+},{
+    card :  "4",
+    suit :  "hearts",
+    value : 4,
+    id : "102",
+    color : "red"
+},{
+    card :  "5",
+    suit :  "hearts",
+    value : 5,
+    id : "103",
+    color : "red"
+},{
+    card :  "6",
+    suit :  "hearts",
+    value : 6,
+    id : "104",
+    color : "red"
+},{
+    card :  "7",
+    suit :  "hearts",
+    value : 7,
+    id : "105",
+    color : "red"
+},{
+    card :  "8",
+    suit :  "hearts",
+    value : 8,
+    id : "106",
+    color : "red"
+},{
+    card :  "9",
+    suit :  "hearts",
+    value : 9,
+    id : "107",
+    color : "red"
+},{
+    card :  "10",
+    suit :  "hearts",
+    value : 10,
+    id : "108",
+    color : "red"
+},{
+    card :  "J",
+    suit :  "hearts",
+    value : 10,
+    id : "109",
+    color : "red"
+},{
+    card :  "Q",
+    suit :  "hearts",
+    value : 10,
+    id : "110",
+    color : "red"
+},{
+    card :  "K",
+    suit :  "hearts",
+    value : 10,
+    id : "111",
+    color : "red"
+},{
+    card :  "A",
+    suit :  "hearts",
+    value : 11,
+    id : "112",
+    color : "red"
+},{
+    card :  "2",
+    suit :  "diamonds",
+    value : 2,
+    id : "113",
+    color : "red"
+},{
+    card :  "3",
+    suit :  "diamonds",
+    value : 3,
+    id : "114",
+    color : "red"
+},{
+    card :  "4",
+    suit :  "diamonds",
+    value : 4,
+    id : "115",
+    color : "red"
+},{
+    card :  "5",
+    suit :  "diamonds",
+    value : 5,
+    id : "116",
+    color : "red"
+},{
+    card :  "6",
+    suit :  "diamonds",
+    value : 6,
+    id : "117",
+    color : "red"
+},{
+    card :  "7",
+    suit :  "diamonds",
+    value : 7,
+    id : "118",
+    color : "red"
+},{
+    card :  "8",
+    suit :  "diamonds",
+    value : 8,
+    id : "119",
+    color : "red"
+},{
+    card :  "9",
+    suit :  "diamonds",
+    value : 9,
+    id : "120",
+    color : "red"
+},{
+    card :  "10",
+    suit :  "diamonds",
+    value : 10,
+    id : "121",
+    color : "red"
+},{
+    card :  "J",
+    suit :  "diamonds",
+    value : 10,
+    id : "122",
+    color : "red"
+},{
+    card :  "Q",
+    suit :  "diamonds",
+    value : 10,
+    id : "123",
+    color : "red"
+},{
+    card :  "K",
+    suit :  "diamonds",
+    value : 10,
+    id : "124",
+    color : "red"
+},{
+    card :  "A",
+    suit :  "diamonds",
+    value : 11,
+    id : "125",
+    color : "red"
+},{
+    card :  "2",
+    suit :  "spades",
+    value : 2,
+    id : "126",
+    color : "black"
+},{
+    card :  "3",
+    suit :  "spades",
+    value : 3,
+    id : "127",
+    color : "black"
+},{
+    card :  "4",
+    suit :  "spades",
+    value : 4,
+    id : "128",
+    color : "black"
+},{
+    card :  "5",
+    suit :  "spades",
+    value : 5,
+    id : "129",
+    color : "black"
+},{
+    card :  "6",
+    suit :  "spades",
+    value : 6,
+    id : "130",
+    color : "black"
+},{
+    card :  "7",
+    suit :  "spades",
+    value : 7,
+    id : "131",
+    color : "black"
+},{
+    card :  "8",
+    suit :  "spades",
+    value : 8,
+    id : "132",
+    color : "black"
+},{
+    card :  "9",
+    suit :  "spades",
+    value : 9,
+    id : "133",
+    color : "black"
+},{
+    card :  "10",
+    suit :  "spades",
+    value : 10,
+    id : "134",
+    color : "black"
+},{
+    card :  "J",
+    suit :  "spades",
+    value : 10,
+    id : "135",
+    color : "black"
+},{
+    card :  "Q",
+    suit :  "spades",
+    value : 10,
+    id : "136",
+    color : "black"
+},{
+    card :  "K",
+    suit :  "spades",
+    value : 10,
+    id : "137",
+    color : "black"
+},{
+    card :  "A",
+    suit :  "spades",
+    value : 11,
+    id : "138",
+    color : "black"
+},{
+    card :  "2",
+    suit :  "clubs",
+    value : 2,
+    id : "139",
+    color : "black"
+},{
+    card :  "3",
+    suit :  "clubs",
+    value : 3,
+    id : "140",
+    color : "black"
+},{
+    card :  "4",
+    suit :  "clubs",
+    value : 4,
+    id : "141",
+    color : "black"
+},{
+    card :  "5",
+    suit :  "clubs",
+    value : 5,
+    id : "142",
+    color : "black"
+},{
+    card :  "6",
+    suit :  "clubs",
+    value : 6,
+    id : "143",
+    color : "black"
+},{
+    card :  "7",
+    suit :  "clubs",
+    value : 7,
+    id : "144",
+    color : "black"
+},{
+    card :  "8",
+    suit :  "clubs",
+    value : 8,
+    id : "145",
+    color : "black"
+},{
+    card :  "9",
+    suit :  "clubs",
+    value : 9,
+    id : "146",
+    color : "black"
+},{
+    card :  "10",
+    suit :  "clubs",
+    value : 10,
+    id : "147",
+    color : "black"
+},{
+    card :  "J",
+    suit :  "clubs",
+    value : 10,
+    id : "148",
+    color : "black"
+},{
+    card :  "Q",
+    suit :  "clubs",
+    value : 10,
+    id : "149",
+    color : "black"
+},{
+    card :  "K",
+    suit :  "clubs",
+    value : 10,
+    id : "150",
+    color : "black"
+},{
+    card :  "A",
+    suit :  "clubs",
+    value : 11,
+    id : "151",
+    color : "black"
+}]
+
+// use [...newDeck] to Clone the value instead of referencing to it
+let deck = [...newDeck]
+
+
+
+let playerSumElement = document.getElementById("playerTotalCountEl")
+let cardsLeftEl = document.getElementById("cardsLeftEl");
+let hitButton = document.getElementById("hit-btn");
+let standButton = document.getElementById("stand-btn");
+let startButton = document.getElementById("start-btn");
+let newDeckButton;
+
+
+
+let myCards = []
+let totalSum = 0;
+
+
+
+// picks a card object from the deck array, removes it  and stores it in pickedCard, pushes it so myCards and Calls renderCard()
+function pick(dealerTurn) {
+    if (totalSum > 21){
+        
+        return
+    }  
+
+    if (deck.length > 0) {
+        let indexNumber = Math.floor(Math.random() * deck.length );
+        // cardEl.textContent = "You have picked " + deck[indexNumber].card + " of " + deck[indexNumber].suit;
+        pickedCard = deck[indexNumber]
+        deck.splice(indexNumber, 1);
+        cardsLeftEl.textContent = ("decksize is now " + deck.length);
+        console.log(pickedCard)
+        myCards.push(pickedCard)
+        renderCardPlayer(pickedCard)
+    } else {
+        cardEl.textContent = "You are out of cards!";
+        newDeckButton = document.createElement("button");
+        newDeckButton.setAttribute("id", "newDeckButton");
+        newDeckButton.setAttribute("onclick", "reshuffle()");
+        newDeckButton.textContent = "Reshuffle Deck";
+        container.appendChild(newDeckButton)    
+        
+}
+        return pickedCard
+        
+        
+        
+}
+
+
+
+function renderCardPlayer(pickedCard){
+        let newCard;
+        let suit;
+        let color;
+        switch (pickedCard.suit) {
+            case "hearts":
+                suit = "♥"
+                color = "red"
+                break;
+            case "spades":
+                suit = "♠"
+                color = "black"
+                break;
+            case "clubs":
+                suit = "♣"
+                color = "black"
+                break;
+            case "diamonds":
+                suit = "♦"
+                color = "red"
+                break;
+            default:
+                break;
+        }
+
+        newCard = document.createElement("div");
+        newCard.setAttribute("class","card");
+        newCard.classList.add(color);
+        newCard.setAttribute("id",pickedCard.id);
+        
+        newCard.textContent = (suit)
+        newCard.textContent += (pickedCard.card)
+        myCardsBoard.appendChild(newCard);
+        sumVal(myCards)
+
+}
+
+// re-adds all the cards to the deck
+function reshuffle(){
+        deck = [...newDeck]
+        cardsLeftEl.textContent = ("decksize is now " + deck.length)
+        newDeckButton.remove();
+}
+
+
+function sumVal(myCards){
+    let sumPlayer = 0;
+       myCards.forEach(myCards => {
+        sumPlayer += (myCards.value);
+       });
+       totalSum = sumPlayer;
+       playerSumElement.textContent = "Your current count is: " + (totalSum) + "!";
+      
+       
+       if (totalSum > 21) {
+        playerSumElement.classList.add("bustedText");
+        playerSumElement.textContent = "Busted! Your total was " + totalSum;
+        hitButton.hidden = true;
+        startButton.classList.remove("displayNone")
+        
+        
+    }
+
+}
+
+
+function startGame(){
+    let deck = [...newDeck];
+    
+    myCards.forEach(cardEntry => {
+        console.log(cardEntry.id)
+        document.getElementById(cardEntry.id).remove()
+    });
+    
+    myCards = [];
+    totalSum = 0
+    
+    
+    //startButton.classList.add("displayNone");
+    pick(false);
+    pick(false);
+    
+
+    hitButton.hidden = false;
+    deck = [...newDeck];
+    
+}
